1db52c6c45ea9fabd9f673ee225201f899d5654d
shivamkaush1k <shivamkaushik.1004@gmail.com>
2026-03-26 10:51:32 +0530

    Fix media file handling for portfolio deployment

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name = "portfolio_app"
 
 urlpatterns = [
     path("", views.home, name="home"),
 ]
-
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
