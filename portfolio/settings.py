1ba1080956c0a531b7ccbc0c4c2de408998dd50b
shivamkaush1k <shivamkaushik.1004@gmail.com>
2026-03-26 12:58:21 +0530

    Update portfolio static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,6 +83,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/data/media'
+MEDIA_ROOT = BASE_DIR / 'media'
